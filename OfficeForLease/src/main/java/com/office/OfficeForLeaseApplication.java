2cc99bed3fc51394d9d99753775edecbcafae503
bpham <aquocbao1340@gmail.com>
2022-05-30 09:53:11 +0700

    update mysql profile

--- OfficeForLease/src/main/java/com/office/OfficeForLeaseApplication.java
+++ OfficeForLease/src/main/java/com/office/OfficeForLeaseApplication.java
@@ -2,12 +2,23 @@ package com.office;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class OfficeForLeaseApplication {
+@RestController
+public class OfficeForLeaseApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OfficeForLeaseApplication.class, args);
 	}
+	
+	  @Bean
+	  PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	  }
 
 }
