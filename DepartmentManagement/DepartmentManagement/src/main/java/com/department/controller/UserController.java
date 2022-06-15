7ab121b87d2b6deec311b56c176965544344fc8a
Thor Le <thorle9x@gmail.com>
2022-06-15 23:02:43 +0700

    fix user controller

--- DepartmentManagement/DepartmentManagement/src/main/java/com/department/controller/UserController.java
+++ DepartmentManagement/DepartmentManagement/src/main/java/com/department/controller/UserController.java
@@ -8,6 +8,8 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,27 +33,20 @@ public class UserController extends AbstractController {
 	@Autowired
 	UserService userService;
 
-//	@ApiOperation(value = "Get All Users", notes = "Get All Users")
-//	@ApiResponses(value = { @ApiResponse(code = 401, response = Response.class, message = "INVALID_TOKEN") })
-//	@GetMapping()
-//	public ResponseEntity<ServerResponse> getUsers() {
-//		return new ResponseEntity<>(new ServerResponse(HttpStatusEnum.SUCCESS).setResult(userService.findAll()),
-//				HttpStatus.OK);
-//	}
-//
-//	@ApiOperation(value = "Get user by id", notes = "Get employee by id")
-//	@ApiResponses(value = { @ApiResponse(code = 401, response = Response.class, message = "INVALID_TOKEN") })
-//	@GetMapping("/{id}")
-//	public ResponseEntity<ServerResponse> findById(@PathVariable Long id) {
-//		UserDTO user = null;
-//		try {
-//			user = userService.findById(id);
-//		} catch (Exception e) {
-//			return new ResponseEntity<>(new ServerResponse(HttpStatusEnum.NO_RECORD_FOUND, id).setResult(user),
-//					HttpStatus.NOT_FOUND);
-//		}
-//		return new ResponseEntity<>(new ServerResponse(HttpStatusEnum.SUCCESS).setResult(user), HttpStatus.OK);
-//	}
+	@ApiOperation(value = "Get All Users", notes = "Get All Users")
+	@ApiResponses(value = { @ApiResponse(code = 401, response = Response.class, message = "INVALID_TOKEN") })
+	@GetMapping()
+	public ResponseEntity<ServerResponse> getUsers() {
+		return new ResponseEntity<>(new ServerResponse(HttpStatusEnum.SUCCESS).setResult(userService.findAll()), HttpStatus.OK);
+	}
+
+	@ApiOperation(value = "Get user by id", notes = "Get employee by id")
+	@ApiResponses(value = { @ApiResponse(code = 401, response = Response.class, message = "INVALID_TOKEN") })
+	@GetMapping("/{id}")
+	public ResponseEntity<ServerResponse> findById(@PathVariable Long id) {
+		UserDTO user = userService.findById(id);
+		return new ResponseEntity<>(new ServerResponse(HttpStatusEnum.SUCCESS).setResult(user), HttpStatus.OK);
+	}
 
 	@ApiOperation(value = "Create new User", notes = "Create new User")
 	@ApiResponses(value = { @ApiResponse(code = 401, response = Response.class, message = "INVALID_TOKEN") })
